451aa83ee50aad239314eb0cd45d11d47a724c6f
Lily Caramel <131236664+LilyCaramel2@users.noreply.github.com>
2025-12-23 21:59:49 +0200

    Delete assets/js directory

--- assets/js/includes.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll("[data-include]").forEach(async (el) => {
-    const name = el.getAttribute("data-include");
-    const res = await fetch(`partials/${name}.html`);
-    if (res.ok) el.innerHTML = await res.text();
-  });
-});
